a3b63cbdce76bf336a40b144796e15c140f95563
pranaysahariah <96043023+pranaysahariah@users.noreply.github.com>
2021-12-13 21:04:39 +0530

    includes

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+ <footer class="foo">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p style="text-align:center; font-family: 'Monotype Corsiva'; font-size:20px;"><i class="material-icons" style="color: black;">COPYRIGHT</i> 2020-21 <b >Online Notes Sharing and Management</a></b></p>
+                </div>
+            </div>
+   </footer>
